fc37def56cf6e7376335fb583f1d6376543f7154
Adam-2000 <452422964@qq.com>
2022-06-03 01:14:18 +0800

    lab11 1.0

--- lab11/lab11/graphs/MazeAStarPath.java
+++ lab11/lab11/graphs/MazeAStarPath.java
@@ -1,5 +1,7 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  *  @author Josh Hug
  */
@@ -20,23 +22,62 @@ public class MazeAStarPath extends MazeExplorer {
 
     /** Estimate of the distance from v to the target. */
     private int h(int v) {
-        return -1;
+        return Math.abs(maze.toX(v) - maze.toX(t)) + Math.abs(maze.toY(v) - maze.toY(t));
     }
 
+    private class Qnode implements Comparable {
+        private final int v;
+        private final int priority;
+        Qnode(int v, int p) {
+            this.v = v;
+            this.priority = p;
+        }
+        @Override
+        public int compareTo(Object o) {
+            if (o == null || o.getClass() != this.getClass()) {
+                return -1;
+            }
+            return this.priority - ((Qnode) o).priority;
+        }
+    }
     /** Finds vertex estimated to be closest to target. */
-    private int findMinimumUnmarked() {
+    private int popMinimumUnmarked() {
         return -1;
-        /* You do not have to use this method. */
     }
 
     /** Performs an A star search from vertex s. */
-    private void astar(int s) {
-        // TODO
+    private void astar() {
+        MinPQ<Qnode> pq = new MinPQ<>();
+        pq.insert(new Qnode(s, 0 + h(s)));
+        marked[s] = true;
+        announce();
+        if (s == t) {
+            return;
+        }
+        while (!pq.isEmpty()) {
+            Qnode node = pq.delMin();
+            int v = node.v;
+            if (v == t) {
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                    if (w == t) {
+                        return;
+                    }
+                    pq.insert(new Qnode(w, distTo[w] + h(w)));
+                }
+            }
+        }
     }
 
     @Override
     public void solve() {
-        astar(s);
+        astar();
     }
 
 }
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,64 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
-
+    private Maze maze;
+    private int targetFound = -1;
+    private int[] oldEdgeTo;
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        oldEdgeTo = new int[m.V()];
+    }
+
+    private void dfs(int v, int parent) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (w != parent) {
+                if (!marked[w]) {
+                    oldEdgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                    dfs(w, v);
+                    if (targetFound != -1) {
+                        return;
+                    }
+                } else {
+                    targetFound = w;
+                    oldEdgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    announce();
+                    return;
+                }
+            }
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        for (int i = 1; i <= maze.N(); i++) {
+            for (int j = 1; j <= maze.N(); j++) {
+                int v = maze.xyTo1D(i, j);
+                if (!marked[v]) {
+                    distTo[v] = 0;
+                    dfs(v, -1);
+                    if (targetFound != -1) {
+                        backTrackCircle();
+                        return;
+                    }
+                }
+            }
+        }
     }
 
     // Helper methods go here
+    private void backTrackCircle() {
+        int curr = edgeTo[targetFound] = oldEdgeTo[targetFound];
+        while (curr != targetFound) {
+            curr = edgeTo[curr] = oldEdgeTo[curr];
+            announce();
+        }
+    }
 }
 
